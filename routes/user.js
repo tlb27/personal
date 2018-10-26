683b40c1d9ef90cd1e6ea6d609feb1c2ceea1735
simple <tlb27@users.noreply.github.com>
2018-10-26 21:07:38 +0800

    Add files via upload

---
+++ routes/user.js
@@ -0,0 +1,39 @@
+const express=require('express');
+//引入mysql连接池
+const pool=require('../pool.js');
+//创建路由器
+var router=express.Router();
+//在路由下添加路由
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exprots=router;
+
+
+
+
+
+
+
